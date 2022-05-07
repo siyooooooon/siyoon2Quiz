ec65dd0bf63948b26e4791f32dfa83d6e0fbd279
Kim-Siyoon <siyooooooon@naver.com>
2022-05-07 14:12:53 +0900

    백준 문제풀이

--- 1day/index.js
+++ 1day/index.js
@@ -1,12 +1,15 @@
 // file system import
 let fs = require('fs');
 // file system 을 이용한 전체 입력 받기
-let input = fs.readFileSync('./1day/test.txt').toString().split('/n');
+let input = fs.readFileSync('./1day/test.txt').toString().trim().split('/n');
 
 let testCase = input[0];
-
-for (let i = 1; i <= testCase; i++) {
-    let numbers = input[i].split(' ');
-    
+let numbers = [];
+for (let i = 0; i <= testCase; i++) {
+    numbers = input[i].split(' ');
+    console.log(testCase);
+    console.log('numbers', numbers);
     console.log(Number(numbers[0]) + Number(numbers[1]));
 }
+
+console.log();
